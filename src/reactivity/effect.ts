08a5e78f7e437ac79ab878debba91062c6a67b6d
qq449245884 <449245884@qq.com>
2021-12-05 00:38:30 +0800

    feat:实现stop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,10 @@
+import { extend } from "../shared";
+
 class ReactiveEffect {
   private _fn: any;
+  deps =[]
+  active = true;
+  onStop?: () => void;
   constructor (fn, public scheduler?) {
     this._fn = fn
   }
@@ -8,7 +13,25 @@ class ReactiveEffect {
     return this._fn()
   }
 
+  stop() {
+    if (this.active) {
+      cleanupEffect( this)
+      if (this.onStop) {
+        this.onStop()
+      }
+    }
+    this.active = false
+  }
+
 }
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
+}
+
+
 const targetMap = new Map()
 export function track(target, key) {
   // target -> key -> dep
@@ -23,7 +46,9 @@ export function track(target, key) {
     dep = new Set()
     depsMap.set(key, dep)
   }
+  if (!activeEffect) return
   dep.add(activeEffect)
+  activeEffect.deps.push(dep)
 }
 
 export function trigger (target, key)  {
@@ -46,6 +71,13 @@ let activeEffect;
 export function effect (fn, options: effectOptions = {}) {
   // fn
   const _effect = new ReactiveEffect(fn, options.scheduler);
+  extend(_effect, options)
    _effect.run()
-   return  _effect.run.bind(_effect)
+   const runner: any =  _effect.run.bind(_effect)
+   runner.effect = _effect
+   return runner
+}
+
+export function stop (runner) {
+  runner.effect.stop()
 }
