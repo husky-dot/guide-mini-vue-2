7256d6b7176c7d6995bead2062de8023a15f60d3
qq449245884 <449245884@qq.com>
2021-12-05 10:26:22 +0800

    feat: 实现 isReadonly 和 isReactive 功能

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,5 +1,9 @@
 import { mutableHandlers, readonlyHandlers } from "./baseHandler"
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = "__v_isReactive",
+  IS_READONLY = "__v_isReadonly",
+}
 
 export function reactive (raw) {
   return createReactiveObject(raw, mutableHandlers)
@@ -8,6 +12,13 @@ export function reactive (raw) {
 
 export  function readonly(raw) {
   return createReactiveObject(raw, readonlyHandlers)
+}
+
+export function isReactive(value) {
+  return !!value[ReactiveFlags.IS_REACTIVE]
+}
+export function isReadonly(value) {
+  return !!value[ReactiveFlags.IS_READONLY]
 }
 
 function createReactiveObject(target, baseHandles) {
