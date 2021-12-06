fb5d32dd1ebb84976735958c5d90452154152d2d
qq449245884 <449245884@qq.com>
2021-12-07 00:31:03 +0800

    feat: 实现初始化 element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -2,7 +2,18 @@ import { h } from "../../lib/guide-mini-vue.esm.js";
 export const App = {
   render() {
     // ui
-    return h("div", "hi, " + this.msg);
+    return h(
+      "div",
+      {
+        id: "root",
+        class: ["red", "hard"],
+      },
+      // "hi, " + this.msg
+      // string
+      // "hi, mini-vue"
+      // Array
+      [h("p", { class:"red"}, "hi"), h("p", {class:"blue"}, "mini-vue")]
+    );
   },
 
   setup() {
--- example/helloworld/main.js
+++ example/helloworld/main.js
@@ -1,4 +1,4 @@
-createApp(App).mount("#app");
+
 import { createApp } from "../../lib/guide-mini-vue.esm.js";
 import { App } from "./App.js";
 
