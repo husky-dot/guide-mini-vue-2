e96f7396bf10cc1d7bc9f0cec5fb6f4513c83135
qq449245884 <449245884@qq.com>
2021-12-09 08:40:39 +0800

    feat: 实现 Fragment 和 Text 类型节点

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,17 +1,40 @@
 import { ShapeFlags } from "../shared/ShapeFlags";
 import { createComponentInstance, setupComponent } from "./component";
+import { Fragment, Text } from "./vnode";
 
 export function render(vnode, container) {
   patch(vnode, container);
 }
 
 function patch(vnode, container) {
-  const { shapeFlag } = vnode;
-  if (typeof vnode.type === "string") {
-    processElement(vnode, container);
-  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
-    processComponent(vnode, container);
+  const { type, shapeFlag } = vnode;
+
+  switch (type) {
+    case Fragment:
+      processFragment(vnode, container);
+      break;
+    case Text:
+      processText(vnode, container);
+      break;
+
+    default:
+      if (shapeFlag & ShapeFlags.ELEMENT) {
+        processElement(vnode, container);
+      } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
+        processComponent(vnode, container);
+      }
+      break;
   }
+}
+
+function processText(vnode: any, container: any) {
+  const { children } = vnode;
+  const textNode = (vnode.el = document.createTextNode(children));
+  container.append(textNode);
+}
+
+function processFragment(vnode: any, container: any) {
+  mountChildren(vnode, container);
 }
 
 function processElement(vnode: any, container: any) {
@@ -68,5 +91,6 @@ function setupRenderEffect(instance: any, initialVNode, container) {
   const subTree = instance.render.call(proxy);
 
   patch(subTree, container);
+
   initialVNode.el = subTree.el;
 }
--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -1,26 +1,34 @@
 import { ShapeFlags } from "../shared/ShapeFlags";
 
+export const Fragment = Symbol("Fragment");
+export const Text = Symbol("Text");
+
 export function createVNode(type, props?, children?) {
   const vnode = {
     type,
     props,
     children,
     shapeFlag: getShapeFlag(type),
-    el: null
+    el: null,
   };
+
   if (typeof children === "string") {
     vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN;
   } else if (Array.isArray(children)) {
     vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN;
   }
 
-  if(vnode.shapeFlag & ShapeFlags.STATEFUL_COMPONENT){
-    if(typeof children === "object"){
-      vnode.shapeFlag |= ShapeFlags.SLOT_CHILDREN
+  if (vnode.shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
+    if (typeof children === "object") {
+      vnode.shapeFlag |= ShapeFlags.SLOT_CHILDREN;
     }
   }
 
   return vnode;
+}
+
+export function createTextVNode(text: string) {
+  return createVNode(Text, {}, text);
 }
 
 function getShapeFlag(type) {
