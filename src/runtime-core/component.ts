c153ab7a8582ec6de58e2d39271b986876751de2
qq449245884 <449245884@qq.com>
2021-12-10 00:36:53 +0800

    feat: 更新element流程搭建

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -1,3 +1,4 @@
+import { proxyRefs } from "..";
 import { shallowReadonly } from "../reactivity/reactive";
 import { emit } from "./componentEmit";
 import { initProps } from "./componentProps";
@@ -13,6 +14,8 @@ export function createComponentInstance(vnode, parent) {
     slots: {},
     provides: parent ? parent.provides : {},
     parent,
+    isMounted: false,
+    subTree: {},
     emit: () => {}
   };
   component.emit = emit.bind(null, component) as any;
@@ -46,7 +49,7 @@ function handleSetupResult(instance, setupResult: any) {
   // function Object
   // TODO function
   if (typeof setupResult === "object") {
-    instance.setupState = setupResult;
+    instance.setupState = proxyRefs(setupResult);
   }
 
   finishComponentSetup(instance);
